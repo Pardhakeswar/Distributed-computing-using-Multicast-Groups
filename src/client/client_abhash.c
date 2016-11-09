82ea89695d4bf4d6dd8f5d535f2c7ab553557b23
gouhegde <gouhegde@cisco.com>
2016-11-09 13:00:51 +0530

    Adding PerformTask functionality

--- src/client/client_abhash.c
+++ src/client/client_abhash.c
@@ -94,9 +94,50 @@ void JoinOrExit(){
     }while(1);
 }
 
-void AcceptTask()
+void PerformTask()
 {
     //to do
+    int sockfd = 0;
+    struct sockaddr_in serv_addr;
+    int i=0;
+    char receive_buff[1024];
+    char ser_ip_addr[16] = "127.0.0.1";
+
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT); 
+
+    do{
+        
+        if(inet_pton(AF_INET, ser_ip_addr, &serv_addr.sin_addr)<=0)
+        {
+            printf("\n inet_pton error occured\n");
+            return;
+        } 
+        
+        if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        {
+           printf("\n Error : Connect Failed \n");
+           return;
+        }
+
+        memset(receive_buff, '\0', sizeof(receive_buff));
+        recv(sockfd, receive_buff, sizeof(receive_buff),0);
+        printf("%s\n",receive_buff);
+        if(strcmp(receive_buff,"JOINED")==0)
+        {
+           printf("In PerformTask JOINED\n");
+        }
+        else
+        {
+            printf("Client Received some work\n");
+
+            printf("Data is : %s\n", receive_buff);
+
+        }
+    }while(1);
+    return;    
 }
 
 
@@ -105,7 +146,7 @@ int main(int argc, char *argv[])
     pthread_t thread1,thread2;
     int  iret1,iret2;
 
-    iret1 = pthread_create( &thread1, NULL, AcceptTask, NULL);
+    iret1 = pthread_create( &thread1, NULL, PerformTask, NULL);
     iret2 = pthread_create( &thread2, NULL, JoinOrExit, NULL);
 
     pthread_join( thread1, NULL);
@@ -114,6 +155,29 @@ int main(int argc, char *argv[])
     printf("Thread 1 returns: %d\n",iret1);
 
     return 0;
+}
+
+
+int * sortNumbers(int *array, int num_of_elements)
+{
+    int indx_i = 0;
+    int indx_j = 0;
+    int temp = 0;
+
+    for (indx_i = 0 ; indx_i < ( num_of_elements - 1 ); indx_i++)
+    {
+        for (indx_j = 0 ; indx_j < num_of_elements - indx_i - 1; indx_j++)
+        {
+            if (array[indx_j] > array[indx_j+1]) /* For decreasing order use < */
+            {
+                temp            = array[indx_j];
+                array[indx_j]   = array[indx_j+1];
+                array[indx_j+1] = temp;
+            }
+        }
+    }
+
+    return array;
 }
 
 
